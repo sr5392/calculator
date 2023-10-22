97405fffd475d986ecd13da9b09f9801b942de41
sr5392 <61883265+sr5392@users.noreply.github.com>
2023-10-22 18:18:50 +0200

    Add logic to buttons

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-let number1, number2;
-let operator;
-let displayValue;
 
+const display = document.querySelector(".calculator-display p");
 
+for (let i = 0; i <= 9; i++) {
+    let button = document.querySelector("#calculator-button-number-" + i)
+    button.addEventListener("click", () => {
+        display.innerText += i;
+    });
+}
+
+const buttonReset = document.querySelector("#calculator-button-reset");
+buttonReset.addEventListener("click", () => {
+    display.innerText = "0";
+});
+
+const buttonDelete = document.querySelector("#calculator-button-delete");
+buttonDelete .addEventListener("click", () => {
+    display.innerText = "0";
+});
+
+const buttonEquals = document.querySelector("#calculator-button-equals");
+buttonEquals.addEventListener("click", () => {
+    display.innerText = operate(operator, number1, number2);
+});
+
+"calculator-button-equals"
 function operate(operator, number1, number2) {
     return operator(number1, number2);
 }
@@ -22,3 +43,4 @@ function divide(number1, number2) {
 function multiply(number1, number2) {
     return number1 * number2;
 }
+
