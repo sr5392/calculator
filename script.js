49c6e5c0d55a147f3362410878a7e3204d7c94a1
sr5392 <61883265+sr5392@users.noreply.github.com>
2023-11-08 21:18:27 +0100

    Update logic

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 
 const display = document.querySelector(".calculator-display p");
 
+let number1 = "";
+let number2 = "";
+let operator = "";
+
 for (let i = 0; i <= 9; i++) {
     let button = document.querySelector("#calculator-button-number-" + i)
     button.addEventListener("click", () => {
-        display.innerText += i;
+        if (operator == "") {
+            number1 += i;
+        } else {
+            number2 += i; 
+        }
+
+        updateDisplay();
     });
 }
 
 const buttonReset = document.querySelector("#calculator-button-reset");
 buttonReset.addEventListener("click", () => {
+    number1 = "";
+    number2 = "";
+    operator = "";
+
     display.innerText = "0";
 });
 
 const buttonDelete = document.querySelector("#calculator-button-delete");
-buttonDelete .addEventListener("click", () => {
-    display.innerText = "0";
+buttonDelete.addEventListener("click", () => {
+    if (operator == "") {
+        number1 = number1.slice(0, -1);
+    } else {
+        number2 = number2.slice(0, -1);
+    }
+
+    updateDisplay();
 });
 
 const buttonEquals = document.querySelector("#calculator-button-equals");
@@ -23,8 +43,70 @@ buttonEquals.addEventListener("click", () => {
     display.innerText = operate(operator, number1, number2);
 });
 
-"calculator-button-equals"
+const buttonAdd = document.querySelector("#calculator-button-add");
+buttonAdd.addEventListener("click", () => {
+    if (number2 != "") {
+        number1 = operate(operator, parseFloat(number1), parseFloat(number2)).toString();
+        number2 = "";
+    }
+
+    operator =  "+";
+
+    updateDisplay();
+});
+
+const buttonSubtract = document.querySelector("#calculator-button-subtract");
+buttonSubtract.addEventListener("click", () => {
+    if (number2 != "") {
+        number1 = operate(operator, parseFloat(number1), parseFloat(number2)).toString();
+        number2 = "";
+    }
+
+    operator =  "-";
+
+    updateDisplay();
+});
+
+const buttonMultiply = document.querySelector("#calculator-button-multiply");
+buttonMultiply.addEventListener("click", () => {
+    if (number2 != "") {
+        number1 = operate(operator, parseFloat(number1), parseFloat(number2)).toString();
+        number2 = "";
+    }
+
+    operator =  "*";
+
+    updateDisplay();
+});
+
+const buttonDivide = document.querySelector("#calculator-button-divide");
+buttonDivide.addEventListener("click", () => {
+    if (number2 != "") {
+        number1 = operate(operator, parseFloat(number1), parseFloat(number2)).toString();
+        number2 = "";
+    }
+
+    operator =  "/";
+
+    updateDisplay();;
+});
+
 function operate(operator, number1, number2) {
+    switch (operator) {
+        case "+":
+            operator = add;
+            break;
+        case "-":
+            operator = subtract;
+            break;
+        case "*": 
+            operator = multiply;
+            break;
+        case "/": 
+            operator = divide;
+            break;
+    }
+
     return operator(number1, number2);
 }
 
@@ -44,3 +126,6 @@ function multiply(number1, number2) {
     return number1 * number2;
 }
 
+function updateDisplay() {
+    display.innerText = number1 + " " + operator + " " + number2;
+}
